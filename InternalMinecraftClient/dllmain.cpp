8d83aaae779ceffd470aa7f2f6576574ac4af5ae
yeemi routh <shade4keal@gmail.com>
2021-10-16 15:56:44 +1100

    Project files

---
+++ InternalMinecraftClient/dllmain.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+void Init(LPVOID lpParam) {
+
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Init, hModule, 0, 0);
+    }
+    return TRUE;
+}
