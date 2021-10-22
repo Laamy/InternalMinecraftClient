3250dbf06141ed8219642a73158ecc7e7d8eb417
yeemi routh <shade4keal@gmail.com>
2021-10-22 20:03:51 +1100

    renamed prams

--- InternalMinecraftClient/SDK/GameMode.h
+++ InternalMinecraftClient/SDK/GameMode.h
@@ -3,18 +3,18 @@
 struct GameMode {
 public: // VTable
 	virtual void destructorGameMode();
-	virtual bool startDestroyBlock(Vector3i* pos, char blockFace, bool param_3);
+	virtual bool startDestroyBlock(Vector3i* pos, char blockFace, bool a3);
 	virtual bool destroyBlock(Vector3i* pos, char blockFace);
-	virtual bool continueDestroyBlock(Vector3i* pos, char blockFace, Vector3* param_3, bool param_4);
+	virtual bool continueDestroyBlock(Vector3i* pos, char blockFace, Vector3* a3, bool a4);
 	virtual void stopDestroyBlock(Vector3i* pos);
 	virtual void startBuildBlock(Vector3i* pos, char blockFace);
-	virtual bool buildBlock(Vector3i* pos, char blockFacee);
+	virtual bool buildBlock(Vector3i* pos, char blockFace);
 	virtual void continueBuildBlock(Vector3i* pos, char blockFace);
 	virtual void stopBuildBlock();
 	virtual void tick();
-	virtual float getPickRange(__int64* inputMode, bool param_2);
+	virtual float getPickRange(__int64* inputMode, bool a2);
 	virtual bool useItem(__int64* itemStack);
-	virtual bool useItemOn(__int64* itemStack, Vector3i* pos, char blockFace, Vector3* param_4, __int64* block);
+	virtual bool useItemOn(__int64* itemStack, Vector3i* pos, char blockFace, Vector3* a4, __int64* block);
 	virtual bool interact(Actor* actor, Vector3* pos);
 	virtual bool attack(Actor* actor);
 	virtual void releaseUsingItem();
