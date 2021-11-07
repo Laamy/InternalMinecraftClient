b8e03499e63a1ad9db5cdb3e8b48650c657f346c
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-11-07 15:16:25 -0500

    Made ExpandScreen return

--- InternalMinecraftClient/ClientBase/Modules/ExpandScreen.h
+++ InternalMinecraftClient/ClientBase/Modules/ExpandScreen.h
@@ -6,8 +6,14 @@
 class ExpandScreen : public Module {
 public:
 	ExpandScreen(std::string cat) : Module(cat, "ExpandScreen", 0x07) {};
-
+	float window;
+	virtual void OnEnable(ClientInstance* ci, Actor* lp) {
+		window = ci->guiData->windowData->renderWindow.x;
+	};
 	void OnTick(ClientInstance* ci) override {
 		ci->guiData->windowData->renderWindow.x = ci->guiData->resolution.x * 2;
+	};
+	virtual void OnDisable(ClientInstance* ci, Actor* lp) {
+		ci->guiData->windowData->renderWindow.x = window;
 	};
 };
