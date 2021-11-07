207fb21f0b230281c0c8a7a90186527191eac5eb
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-07 13:58:25 +0100

    Create Jesus.h

---
+++ InternalMinecraftClient/ClientBase/Modules/Jesus.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Jesus : public Module {
+public:
+    Jesus(std::string cat) : Module(cat, "Jesus", 0x07) {};
+
+    void OnGameTick(Actor* lp) override {
+        if (lp->IsInWater || lp->IsInLava)
+        {
+            lp->Velocity.y = 0.2f;
+            lp->onGround = true;
+        }
+    };
+};
