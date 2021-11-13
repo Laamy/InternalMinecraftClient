36d9097e9fc457b61b7495ff5f356d45f1b3b8aa
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-13 22:21:52 +0100

    Freaking beautiful ArrayList.
    i made it look clean 😅

--- InternalMinecraftClient/ClientBase/Modules/ArrayList.h
+++ InternalMinecraftClient/ClientBase/Modules/ArrayList.h
@@ -16,7 +16,15 @@ public:
 
 			strPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 4;
 			strPos.y = offset * 24;
+			
+			auto boxPos = Vector2(0, 0);
 
+			boxPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 4;
+			boxPos.y = offset * 9;
+			
+			
+			ctx->Draw(Vector2(boxPos.x - 3, boxPos.y), Vector2(1, 10), _RGB(240, 172, 55, 69));
+			ctx->Draw(boxPos, Vector2(ctx->ctx->getLineLength(ctx->font, &vText1, 1) + 1, 10), _RGB(33, 33, 33, 69));
 			ctx->DrawString(strPos, _RGB(200, 200, 200), vText1, ctx->font);
 			offset++;
 		}
