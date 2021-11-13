afdb320cb16ccbdb4a94d827219e584539042f9c
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-13 21:34:29 +0100

    Update ModuleHandler.h

--- InternalMinecraftClient/ClientBase/ModuleHandler.h
+++ InternalMinecraftClient/ClientBase/ModuleHandler.h
@@ -35,6 +35,7 @@
 #include "Modules/NoFall.h"
 #include "Modules/SpinAttack.h"
 #include "Modules/LifeboatFly.h"
+#include "Modules/NoYFlight.h"
 #include "Modules/Seedhack.h"
 
 class ModuleHandler {
@@ -71,7 +72,7 @@ public:
         // Reach -- I wonder how you would go about reach internally lol? i think i know how actually i would sig scan then covert it to a float*
 
         modules.push_back(new AirStuck(player));
-        modules.push_back(new AutoWalk(player));//needs a fix for using Cos,...
+        modules.push_back(new AutoWalk(player));
         modules.push_back(new FastWater(player));
         modules.push_back(new Glide(player));
         modules.push_back(new HighJump(player));
@@ -115,9 +116,10 @@ public:
         // Flight
         // HiveFlight
         // Jetpack
-        modules.push_back(new OGMFlight(flies));//needs a fix for using Cos,...
-        modules.push_back(new LifeboatFly(flies));
+        //modules.push_back(new OGMFlight(flies));
+        //modules.push_back(new LifeboatFly(flies));
         modules.push_back(new CreativeFly(flies));
+        modules.push_back(new NoYFlight(flies));
         // TeleportPhase
 
         modules.push_back(new Watermark(visual));
