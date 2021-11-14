e77cac3aecd639716c57aef264436a7cff678707
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-11-14 14:25:15 -0500

    Fix NoPacket crash when in main menu

--- InternalMinecraftClient/ClientBase/Modules/NoPacket.h
+++ InternalMinecraftClient/ClientBase/Modules/NoPacket.h
@@ -6,12 +6,16 @@ public:
 	Vector3 savedPos;
 
 	void OnEnable(ClientInstance* ci, Actor* lp) override {
+		if (lp == nullptr) return;
+
 		savedPos = lp->Position.lower;
 
 		ci->loopbackSender->RetPacketSender();
 	};
 
 	void OnDisable(ClientInstance* ci, Actor* lp) override {
+		if (lp == nullptr) return;
+
 		lp->SetPos(savedPos);
 
 		ci->loopbackSender->RestorePacketSender();
