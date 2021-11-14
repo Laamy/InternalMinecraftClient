49d62569d67fad2ed73695cee0ad0f762f465f97
Omikron <jcazman19@yahoo.com>
2021-11-14 10:52:07 -0500

    Good jesus

--- InternalMinecraftClient/ClientBase/Modules/Jesus.h
+++ InternalMinecraftClient/ClientBase/Modules/Jesus.h
@@ -5,10 +5,9 @@ public:
     Jesus(std::string cat) : Module(cat, "Jesus", 0x07) {};
 
     void OnGameTick(Actor* lp) override {
-        if (lp->IsInWater || lp->IsInLava)
-        {
-            lp->Velocity.y = 0.2f;
-            lp->onGround = true;
+        if (lp->IsInWater || lp->IsInLava) {
+            lp->Velocity.y = 0.02f;
+            lp->onGround = 1;
         }
     };
 };
