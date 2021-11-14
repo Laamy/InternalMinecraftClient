3214080c975e9536fb9e707047425465ca72ebb2
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-11-14 14:28:51 -0500

    Fix SpinAttack crash when in main menu

--- InternalMinecraftClient/ClientBase/Modules/SpinAttack.h
+++ InternalMinecraftClient/ClientBase/Modules/SpinAttack.h
@@ -3,16 +3,19 @@
 class SpinAttack : public Module {
 public:
 	SpinAttack(std::string cat) : Module(cat, "SpinAttack", 0x07) {};
-
+	vLocalPlayer* player;
 	void OnEnable(ClientInstance* ci, Actor* lp) override {
-		lp->startSpinAttack();
+		if (lp == nullptr) return;
+		player->startSpinAttack();
 	};
 
 	void OnGameTick(Actor* lp) override {
-		lp->startSpinAttack();
+		if (lp == nullptr) return;
+		player->startSpinAttack();
 	};
 
 	void OnDisable(ClientInstance* ci, Actor* lp) override {
-		lp->stopSpinAttack();
+		if (lp == nullptr) return;
+		player->stopSpinAttack();
 	};
 };
