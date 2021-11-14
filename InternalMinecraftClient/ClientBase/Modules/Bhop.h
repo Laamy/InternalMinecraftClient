a4b6d8779c8521748678fbbe995b9858034f9705
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-14 12:00:53 +0100

    Bhop stable now

--- InternalMinecraftClient/ClientBase/Modules/Bhop.h
+++ InternalMinecraftClient/ClientBase/Modules/Bhop.h
@@ -2,54 +2,16 @@
 
 class Bhop : public Module {
 public:
-    Bhop(std::string cat) : Module(cat, "Bhop", 0x07) {};
-	  ClientInstance* ci;
-    int jumpDelay = 0;
+	Bhop(std::string cat) : Module(cat, "Bhop", 0x07) {};
+	float speed = 0.64;
 
-	void OnEnable(ClientInstance* ci, Actor* lp) override {
-		if (lp == nullptr) return;
-		this->ci = ci;
+	void OnGameTick(Actor* lp) override {
+		float yaw = (float)lp->bodyRots()->y;
+
+		lp->Velocity.z = sin((yaw + 90) * 0.01745329251f) * speed;
+		lp->Velocity.x = cos((yaw + 90) * 0.01745329251f) * speed;
+		if (lp->onGround) {
+			lp->Velocity.y = 0.4;
+		}
 	};
-
-
-    void OnGameTick(Actor* lp)
-    {
-        if (lp == nullptr) return;
-
-        if (!lp->onGround2)
-        {
-            Vector3 newVel = lp->Velocity;
-
-            auto cy = (lp->bodyRots()->y + 89.9) * ((float)PI / 180);
-
-            if (keymap['W'] || keymap['A'] || keymap['S'] || keymap['D'])
-            {
-                if (keymap['A'])
-                    cy += 55;
-                if (keymap['S'])
-                    cy += 110;
-                if (keymap['D'])
-                    cy -= 55;
-
-                int speed = 7;
-
-                newVel.z = (float)sin(cy) * (speed / 2);
-                newVel.x = (float)cos(cy) * (speed / 2);
-
-                lp->Velocity = newVel;
-
-                if (jumpDelay != 0)
-                    jumpDelay = 0;
-            }
-        }
-        else
-        {
-            if (keymap['W'] || keymap['A'] || keymap['S'] || keymap['D'])
-            {
-                jumpDelay++;
-                if (jumpDelay == 2)
-                    lp->Velocity.y = 0.4;
-            }
-        }
-    }//it works really bad, but i didnt have lots of time to make it work good, prob gonna make it better another time
 };
