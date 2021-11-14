100a6e2e88bf8fec79aa6af4b35eb394dfb65ab9
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-14 12:24:54 +0100

    Position fix?

--- InternalMinecraftClient/ClientBase/Modules/ArrayList.h
+++ InternalMinecraftClient/ClientBase/Modules/ArrayList.h
@@ -3,7 +3,6 @@
 class ArrayList : public Module {
 public:
 	ArrayList(std::string cat) : Module(cat, "ArrayList", 0x07) {};
-	float speed = 0.4;
 
 	void OnFrameRender(RenderUtils* ctx) override {
 		int offset = 0;
@@ -14,18 +13,18 @@ public:
 
 			auto strPos = Vector2(0, 0);
 
-			strPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 4;
-			strPos.y = offset * 24;
-			
+			strPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 0;
+			strPos.y = offset * 20;
+
 			auto boxPos = Vector2(0, 0);
 
-			boxPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 4;
-			boxPos.y = offset * 9;
-			
-			
-			ctx->Draw(Vector2(boxPos.x - 3, boxPos.y), Vector2(1, 10), _RGB(240, 172, 55, 69));
-			ctx->Draw(boxPos, Vector2(ctx->ctx->getLineLength(ctx->font, &vText1, 1) + 1, 10), _RGB(33, 33, 33, 69));
+			boxPos.x = ctx->guiData->scaledResolution.x - ctx->ctx->getLineLength(ctx->font, &vText1, 1) - 1;
+			boxPos.y = offset * 10;
+
 			ctx->DrawString(strPos, _RGB(200, 200, 200), vText1, ctx->font);
+			ctx->Draw(Vector2(boxPos.x - 2, boxPos.y), Vector2(2, 10), _RGB(255, 0, 204));//Why does it appear black?
+			ctx->Draw(boxPos, Vector2(ctx->ctx->getLineLength(ctx->font, &vText1, 1) + 1, 10), _RGB(255, 0, 204, 69));//why does everything appear black?
+			
 			offset++;
 		}
 	}
