2e25f24364b62b16090876f5493cc2a25a6acf43
Intop <78622918+Intoprelised@users.noreply.github.com>
2021-11-16 12:45:11 -0500

    oops

--- InternalMinecraftClient/ClientBase/Modules/HighJump.h
+++ InternalMinecraftClient/ClientBase/Modules/HighJump.h
@@ -5,7 +5,7 @@ public:
     HighJump(std::string cat) : Module(cat, "HighJump", 0x07) {};
 
     void OnGameTick(Actor* lp) override {
-        if (lp->onGround && keymap[(int)' '])
+        if (lp->onGround2 && keymap[(int)' '])
             lp->Velocity.y = 1.0f;
     }
 };
