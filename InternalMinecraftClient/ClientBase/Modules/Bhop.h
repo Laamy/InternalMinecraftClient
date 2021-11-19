55168cf4da32be3b7e863a6e5853c01c34bd148e
Omikron <jcazman19@yahoo.com>
2021-11-19 17:00:19 -0500

    The Most Simple Shit

--- InternalMinecraftClient/ClientBase/Modules/Bhop.h
+++ InternalMinecraftClient/ClientBase/Modules/Bhop.h
@@ -6,11 +6,33 @@ public:
 
 	void OnGameTick(Actor* lp) override {
 		float yaw = (float)lp->bodyRots()->y;
-
-		lp->Velocity.z = sin((yaw + 90) * 0.01745329251f) * 0.64;
-		lp->Velocity.x = cos((yaw + 90) * 0.01745329251f) * 0.64;
-		if (lp->onGround) {
-			lp->Velocity.y = 0.4;
+		if (keymap['W']) {
+			lp->Velocity.z = sin((yaw + 90) * 0.01745329251f) * 0.44;
+			lp->Velocity.x = cos((yaw + 90) * 0.01745329251f) * 0.44;
+			if (lp->onGround) {
+				lp->Velocity.y = 0.40;
+			}
+		}
+		if (keymap['A']) {
+			lp->Velocity.z = sin((yaw + 0) * 0.01745329251f) * 0.44;
+			lp->Velocity.x = cos((yaw + 0) * 0.01745329251f) * 0.44;
+			if (lp->onGround) {
+				lp->Velocity.y = 0.40;
+			}
+		}
+		if (keymap['S']) {
+			lp->Velocity.z = sin((yaw - 90) * 0.01745329251f) * 0.44;
+			lp->Velocity.x = cos((yaw - 90) * 0.01745329251f) * 0.44;
+			if (lp->onGround) {
+				lp->Velocity.y = 0.40;
+			}
+		}
+		if (keymap['D']) {
+			lp->Velocity.z = sin((yaw + 180) * 0.01745329251f) * 0.44;
+			lp->Velocity.x = cos((yaw + 180) * 0.01745329251f) * 0.44;
+			if (lp->onGround) {
+				lp->Velocity.y = 0.40;
+			}
 		}
 	}
 };
