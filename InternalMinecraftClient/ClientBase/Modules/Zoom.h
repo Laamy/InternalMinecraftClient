acfac49e9915db8acc6250636d13a692c8592e06
NRG <jcazman19@yahoo.com>
2021-11-21 16:42:36 -0500

    Zoom bening gaie

--- InternalMinecraftClient/ClientBase/Modules/Zoom.h
+++ InternalMinecraftClient/ClientBase/Modules/Zoom.h
@@ -3,16 +3,9 @@
 class Zoom : public Module { // worked
 public:
 	Zoom(std::string cat) : Module(cat, "Zoom", 'C') {};
-	bool HoldMode() {
-		return true;
-	}
 	void OnGameTick(Actor* lp) override {
-		if (lp->CameraRots.x == 0) return;
-
-		if (keymap['C'])
+		if (lp == nullptr) return;
 			lp->SetFieldOfView(0.25f);
-		else
-			lp->SetFieldOfView(1);
 	}
 
 	void OnDisable(ClientInstance* ci, Actor* lp) override {
