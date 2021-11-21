e0c9f32166d782adc05c9398031d97cd7b540af3
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-21 21:53:14 +0100

    Update TestCommand.h

--- InternalMinecraftClient/ClientBase/Commands/TestCommand.h
+++ InternalMinecraftClient/ClientBase/Commands/TestCommand.h
@@ -2,7 +2,7 @@
 
 class TestModule : public Command {
 public:
-	TestModule(std::string name) : Command("test", "Pro test cmd", "") {};
+	TestModule(std::string name) : Command("test", "Pro test cmd") {};
 
 	void Execute(ClientInstance* ci, Actor* lp) override {
 	}
