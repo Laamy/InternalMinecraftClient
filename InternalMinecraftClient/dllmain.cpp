ee664a151fee350ee7bafdee75c566815133b210
NRG <jcazman19@yahoo.com>
2021-11-20 20:25:15 -0500

    Ill stop making dum commits now

--- InternalMinecraftClient/dllmain.cpp
+++ InternalMinecraftClient/dllmain.cpp
@@ -62,7 +62,7 @@ blockRenderer _renderBlock;
 typedef bool(__thiscall* Immobile)(Actor* lp);
 Immobile _Immobile;
 
-typedef bool(__thiscall* test)(__int64 _this, int a2, float a3);
+typedef bool(__thiscall* test)(__int64 _this, float* color, __int64 a3, float a4);
 test _Test;
 
 typedef void(__thiscall* render)(void* a1, MinecraftUIRenderContext* ctx);
@@ -232,14 +232,16 @@ bool MobImmobile(Actor* lp) {
     return _Immobile(lp);
 };
 
-bool Test(__int64 test1, int test2, float test3) {
+bool Test(__int64 _this, float* color, __int64 a3, float a4) {
+    static float rcolors[4];
+
     for (auto mod : handler.modules) {
         auto test = mod->name == "TestModule";
-        if (test && mod->enabled) { // there has to be a better way to do this
-            return true;
+        if (test && mod->enabled) {
+            return true;// there has to be a better way to do this
         }
     }
-    return _Test(test1, test2, test3);
+    return _Test(_this, color, a3, a4);
 };
 
 void SendChatMsg(const char txt[64]) { // i was testing please ignore!
@@ -319,7 +321,7 @@ void Init(LPVOID c) {
 
         // Function hooks
         uintptr_t keymapAddr = Mem::findSig("48 89 5C 24 08 57 48 83 EC ? 8B 05 ? ? ? ? 8B DA 89");
-        uintptr_t testAddr = Mem::findSig("44 8B C2 B8 F1 19 76 05 F7 EA");
+        uintptr_t testAddr = Mem::findSig("41 0F 10 08 48 8B C2 0F");
         uintptr_t hookAddr = Mem::findSig("48 8B 01 48 8D 54 24 ? FF 90 ? ? ? ? 90 48 8B 08 48 85 ? 0F 84 ? ? ? ? 48 8B 58 08 48 85 DB 74 0B F0 FF 43 08 48 8B 08 48 8B 58 08 48 89 4C 24 20 48 89 5C 24 28 48 8B 09 48 8B 01 4C 8B C7 48 8B");
         uintptr_t localPlayerAddr = Mem::findSig("F3 0F 10 81 ? ? ? ? 41 0F 2F 00"); //VV - 83 7B 4C 01 75 1C 80 7B
         //uintptr_t displayObjAddr = Mem::findSig("48 89 5C 24 ? 48 89 74 24 ? 55 57 41 54 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 30 4C 8B F1");
