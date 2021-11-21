7bedd12afe5800b2243f91c60900a387c2695b0a
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-11-21 15:37:57 -0500

    Pearl fix your code please

--- InternalMinecraftClient/ClientBase/CommandHandler.h
+++ InternalMinecraftClient/ClientBase/CommandHandler.h
@@ -9,19 +9,17 @@ public:
     std::vector<Command*> commands;
 
 public:
-
     void InitCommands() {
-
         //commands.push_back(new TestCommand("test", "Test command", ""));
     }
-	
-    auto findCommand(std::string name) -> Command* {
+
+    auto findCommand(std::string name)->Command* {
         std::vector<Command*>* allCmds = commands;
-	for (auto cmd : *allCmds) {
-		if (cmd->CheckName(name)) {
-		    return cmd;
-		}
-	    }
+        for (auto cmd : *allCmds) {
+            if (cmd->CheckName(name)) {
+                return cmd;
+            }
+        }
 	return nullptr;
     }
 };
