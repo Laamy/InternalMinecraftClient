46418f10a87c3b40f5e6a1e4137db3cf37d6e842
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-21 21:11:41 +0100

    Create Command.h

---
+++ InternalMinecraftClient/ClientBase/Command.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include "SDK/Actor.h"
+#include "SDK/ClientInstance.h"
+
+class Command {
+public:
+	Command(std::string name, std::string description, std::string alias) { // Constructor
+		this->name = name;
+		this->description = description;
+		this->alias = alias;
+	};
+
+public:
+	std::string name;
+	std::string description;
+	std::string alias;
+
+
+public: // functions
+	virtual void Execute(ClientInstance* ci, Actor* lp) {};
+};
