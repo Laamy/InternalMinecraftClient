052aecdca7ce67af263a3807d1f3e9b9aea1b82a
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-21 21:25:42 +0100

    Update Command.h

--- InternalMinecraftClient/ClientBase/Command.h
+++ InternalMinecraftClient/ClientBase/Command.h
@@ -2,18 +2,31 @@
 
 class Command {
 public:
-	Command(std::string name, std::string description, std::string alias) { // Constructor
+	Command(std::string name, std::string description, std::vector<std::string> aliases) { // Constructor
 		this->name = name;
 		this->description = description;
-		this->alias = alias;
+		this->aliases = aliases;
 	};
 
 public:
 	std::string name;
 	std::string description;
-	std::string alias;
+	std::vector<std::string> aliases
 
 
 public: // functions
 	virtual void Execute(ClientInstance* ci, Actor* lp) {};
+	virtual auto CheckName(std::string Name) -> bool {
+	    if(name == this->name) {
+		return true;
+	    }
+
+	    std::vector<std::string> aliases = aliases;
+	    for(auto alias : aliases) {
+		if(alias == name) {
+			return true;
+		}
+	    }
+	return false;
+    }
 };
