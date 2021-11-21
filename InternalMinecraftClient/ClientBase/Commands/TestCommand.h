3f98c3ed39d26123dcbbf368d688fc0ccb6875cc
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-11-21 23:43:04 +0100

    Update TestCommand.h

--- InternalMinecraftClient/ClientBase/Commands/TestCommand.h
+++ InternalMinecraftClient/ClientBase/Commands/TestCommand.h
@@ -2,12 +2,9 @@
 
 class TestCommand : public Command {
 public:
-	TestCommand(std::string name) : Command(name, "Pro") {};
+	TestCommand(std::string name) : Command(name, "test cmd") {};
 
 	void Execute(ClientInstance* ci, Actor* lp) override {
-		void* a1{};
-		auto cse = TextHolder("[TreroInternal]: FUCK");
-		_chatMsg(a1, &cse);
+		//add whatever you want here
 	}
-
 };
