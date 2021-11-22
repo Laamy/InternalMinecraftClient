3a4d8a25701682cec8d9bdbf0e8c174c9d50a4c8
NRG <jcazman19@yahoo.com>
2021-11-21 22:53:28 -0500

    ANTI KNOCKBACK WOOOOOOOOOOOO

--- InternalMinecraftClient/ClientBase/Modules/Velocity.h
+++ InternalMinecraftClient/ClientBase/Modules/Velocity.h
@@ -3,9 +3,4 @@
 class Velocity : public Module {
 public:
 	Velocity(std::string cat) : Module(cat, "Velocity", 0x0) {};
-	void OnGameTick(Actor* lp) override {
-		auto player = clientInst->getCPlayer();
-		if (player->HurtTime >= 1)//dosent work :/
-			player->Velocity = {0, 0, 0};
-	}
 };
