e61686a52702711ac8a454acccf3835e3043b06f
NRG <jcazman19@yahoo.com>
2021-11-21 21:33:18 -0500

    Added a lerp to Vector3

--- InternalMinecraftClient/ClientBase/Modules/Velocity.h
+++ InternalMinecraftClient/ClientBase/Modules/Velocity.h
@@ -5,7 +5,7 @@ public:
 	Velocity(std::string cat) : Module(cat, "Velocity", 0x0) {};
 	void OnGameTick(Actor* lp) override {
 		auto player = clientInst->getCPlayer();
-		if (player->HurtTime >= 1)
+		if (player->HurtTime >= 1)//dosent work :/
 			player->Velocity = {0, 0, 0};
 	}
 };
