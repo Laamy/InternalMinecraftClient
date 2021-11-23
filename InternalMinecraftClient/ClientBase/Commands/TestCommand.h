b9dd9c3e75a8ac13738740a5f5e65d26b51ca41c
yeemi routh <shade4keal@gmail.com>
2021-11-24 06:07:22 +1100

    test command

--- InternalMinecraftClient/ClientBase/Commands/TestCommand.h
+++ InternalMinecraftClient/ClientBase/Commands/TestCommand.h
@@ -5,15 +5,6 @@ public:
 	TestCommand(std::string name) : Command(name, "test cmd") {};
 
 	void Execute(std::vector<std::string> args) override {
-		/*auto player = clientInst->getCPlayer();
-		Vector3 pos;
-		std::to_string(pos.x);
-		std::to_string(pos.y);
-		std::to_string(pos.z);
-		pos.x = args.at(1);
-		pos.y = args.at(2);
-		pos.z = args.at(3);
-
-		player->setPos(pos);*/
+		hooks->debugEcho("TestEcho", "Test command!");
 	}
 };
