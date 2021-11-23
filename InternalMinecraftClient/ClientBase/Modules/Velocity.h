91c7d4870964faef99cd6fbe4200f7cefee0e5c3
NRG <jcazman19@yahoo.com>
2021-11-23 11:33:25 -0500

    MY VELOCITY WORKS!!!!!

--- InternalMinecraftClient/ClientBase/Modules/Velocity.h
+++ InternalMinecraftClient/ClientBase/Modules/Velocity.h
@@ -2,5 +2,22 @@
 
 class Velocity : public Module {
 public:
-	Velocity(std::string cat) : Module(cat, "Velocity", 0x0) {};
+	VirtualFuncHook* func;
+	Velocity(std::string cat) : Module(cat, "Velocity", 0x07) {
+		uintptr_t address = Mem::findSig("89 81 ? ? ? ? 8B 42 ? 89 81 ? ? ? ? 8B 42 ? 89 81 ? ? ? ? C3 CC CC CC CC CC 48 89 5C 24");
+		func = hooks->createHook("Velocity", address, AntiKB);
+
+	}
+
+	void OnEnable(ClientInstance* a1, Actor* a2) override {
+		func->enableHook();
+	}
+
+	void OnDisable(ClientInstance* a1, Actor* a2) override {
+		func->enableHook(false);
+	}
+
+	static bool AntiKB(Actor* lp) {
+		return false;
+	}
 };
--- InternalMinecraftClient/dllmain.cpp
+++ InternalMinecraftClient/dllmain.cpp
@@ -70,8 +70,6 @@ player _player;
 typedef void(__thiscall* key)(uint64_t keyId, bool held);
 key _key;
 
-typedef bool(__thiscall* VelocityYes)(float Velx, float Vely, float Velz);
-VelocityYes _Vel;
 
 typedef bool(__thiscall* test)(__int64 _this, float* color, __int64 a3, float a4);
 test _Test;
@@ -261,11 +259,6 @@ void playerCallback(Actor* lp, void* a2) {
             mod->OnGameTick(lp);
 };
 
-bool Velocity(float Velx, float Vely, float Velz) {
-   // return false; //for anti knockback!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    return _Vel(Velx, Vely, Velz);
-};
-
 bool Test(__int64 _this, float* color, __int64 a3, float a4) { // There is - Yaami.
     // Initialize these hooks in the module constructor {};
     // I'll preplace the comment for where you need to place this hook code
@@ -386,7 +379,6 @@ void Init(LPVOID c) {
         // Function hooks
         uintptr_t keymapAddr = Mem::findSig("48 89 5C 24 08 57 48 83 EC ? 8B 05 ? ? ? ? 8B DA 89");
         uintptr_t testAddr = Mem::findSig("41 0F 10 08 48 8B C2 0F"); // fog color
-        uintptr_t velocityAddr = Mem::findSig("89 81 ? ? ? ? 8B 42 ? 89 81 ? ? ? ? 8B 42 ? 89 81 ? ? ? ? C3 CC CC CC CC CC 48 89 5C 24");
         uintptr_t hookAddr = Mem::findSig("48 8B 01 48 8D 54 24 ? FF 90 ? ? ? ? 90 48 8B 08 48 85 ? 0F 84 ? ? ? ? 48 8B 58 08 48 85 DB 74 0B F0 FF 43 08 48 8B 08 48 8B 58 08 48 89 4C 24 20 48 89 5C 24 28 48 8B 09 48 8B 01 4C 8B C7 48 8B");
         uintptr_t localPlayerAddr = Mem::findSig("F3 0F 10 81 ? ? ? ? 41 0F 2F 00"); //VV - 83 7B 4C 01 75 1C 80 7B
         //uintptr_t displayObjAddr = Mem::findSig("48 89 5C 24 ? 48 89 74 24 ? 55 57 41 54 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 30 4C 8B F1");
@@ -404,11 +396,6 @@ void Init(LPVOID c) {
         if (MH_CreateHook((void*)testAddr, &Test, reinterpret_cast<LPVOID*>(&_Test)) == MH_OK) {
             MH_EnableHook((void*)testAddr);
             _logf(L"[TreroInternal]: Test hooked!\n");
-        };
-
-        if (MH_CreateHook((void*)velocityAddr, &Velocity, reinterpret_cast<LPVOID*>(&_Vel)) == MH_OK) {
-            MH_EnableHook((void*)velocityAddr);
-            _logf(L"[TreroInternal]: Velocity hooked!\n");
         };
 
         if (MH_CreateHook((void*)keymapAddr, &keyCallback, reinterpret_cast<LPVOID*>(&_key)) == MH_OK) {
