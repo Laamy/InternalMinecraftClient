bf46634e5c3f3b497e025413bf0fa608b8ca9668
NRG <jcazman19@yahoo.com>
2021-11-23 12:47:12 -0500

    Cleaned Up the client

--- InternalMinecraftClient/ClientBase/Modules/Zoom.h
+++ InternalMinecraftClient/ClientBase/Modules/Zoom.h
@@ -1,20 +1,21 @@
 #pragma once
 
-class Zoom : public Module { // worked
+class Zoom : public Module {
 public:
 	Zoom(std::string cat) : Module(cat, "Zoom", 'X') {};//We need to make it so when we are typing in chat, it dosent activate modules hgdgdfhgdfh
 
 	void OnEnable(ClientInstance* ci, Actor* lp) override {
-		if (lp == nullptr) return;
 			lp->SetFieldOfView(0.25f);
+				cancelUiRender = true;
 	}
 
 	void OnDisable(ClientInstance* ci, Actor* lp) override {
-		if (lp == nullptr) return;
 		lp->SetFieldOfView(1);
+		cancelUiRender = false;
 	}
 
 	void OnGameTick(Actor* lp) override {
 		lp->SetFieldOfView(1);
+		lp->SetFieldOfView(0.25f);
 	}
 };
