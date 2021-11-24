474d6cb0d77c671edc69be4b94bc0c5b5923bd81
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-11-24 17:42:52 -0500

    Update StreamName.h

--- InternalMinecraftClient/ClientBase/Modules/StreamName.h
+++ InternalMinecraftClient/ClientBase/Modules/StreamName.h
@@ -5,9 +5,14 @@ public:
 	StreamName(std::string cat) : Module(cat, "StreamName", "Client sidedly hide your minecraft username", 0x07) {
 		// Constructor (Runs once on start up)
 	}
-
+	std::string OGName;
+	void OnEnable(ClientInstance* ci, Actor* lp) {
+		OGName = lp->Username;
+	}
 	void OnGameTick(Actor* a1) override {
-
 		a1->Username = "Streaming";
+	}
+	void OnDisable(ClientInstance* ci, Actor* lp) {
+		lp->Username = OGName;
 	}
 };
