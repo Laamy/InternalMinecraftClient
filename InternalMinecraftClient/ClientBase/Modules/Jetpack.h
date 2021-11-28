f52e38d5905be750cc23c26975c4db4aa7800c55
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-11-28 12:07:06 -0500

    bruh the shid is all over me wtf its in my underwear

--- InternalMinecraftClient/ClientBase/Modules/Jetpack.h
+++ InternalMinecraftClient/ClientBase/Modules/Jetpack.h
@@ -11,8 +11,6 @@ public:
 			auto calcYaw = (lp->bodyRots()->y + 90) * ((float)PI / 180);
 			auto calcPitch = (lp->bodyRots()->x) * -((float)PI / 180);
 
-			lp->Velocity.x = (float)cos(calcYaw) * 1.5f;
-			lp->Velocity.y = (float)sin(calcPitch) * 1.5f;
-			lp->Velocity.z = (float)sin(calcYaw) * 1.5f;
+			lp->lerpybutworky(Vector3((float)cos(calcYaw) * 1.5f, (float)sin(calcPitch) * 1.5f, (float)sin(calcYaw) * 1.5f));
 	}
 };
