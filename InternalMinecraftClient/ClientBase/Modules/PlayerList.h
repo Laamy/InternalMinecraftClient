e615ea1f8908c8f79c9525eb344f3c8f99499905
NRG <jcazman19@yahoo.com>
2021-11-29 18:50:31 -0500

    Daddy Help Me Im Stuck!!!!!!!!!!!!!!!!!!!!!!

--- InternalMinecraftClient/ClientBase/Modules/PlayerList.h
+++ InternalMinecraftClient/ClientBase/Modules/PlayerList.h
@@ -9,15 +9,20 @@ public:
 			int index = 0;
 			for (auto ent : clientInst->getEntityList()) {
 				auto entity = ent.second;
+				auto posx = std::to_string((int)entity->Position.lower.x);
+				auto posy = std::to_string((int)entity->Position.lower.y);
+				auto posz = std::to_string((int)entity->Position.lower.z);
 					ctx->DrawString(Vector2(10, 10 + (index * 20)), _RGB(), TextHolder(
 						std::string(
-							std::string("Found Entity: ") +
-							std::to_string((int)entity->Position.lower.x) + ", " +
-							std::to_string((int)entity->Position.lower.y) + ", " +
-							std::to_string((int)entity->Position.lower.z)
+							std::string("Found Player: ") +
+							posx + ", " +
+							posy + ", " +
+							posz
 						).c_str()
 					), ctx->font);
 					index++;
+					//if (/*Do the check for if its all 0's, it will get rid of it pls*/)
+						//index - 1;
 			}
 		}
 	}
