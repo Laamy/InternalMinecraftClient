f687317d1bff7699d38973748779d49a3db44867
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-11-29 14:32:29 +0300

    fix

--- InternalMinecraftClient/SDK/ClientInstance.h
+++ InternalMinecraftClient/SDK/ClientInstance.h
@@ -54,143 +54,143 @@ public:
 	void RestorePacketSender() {
 		subClass.subClass.subClass.RestorePacketSender();
 	}
-}; // this 100% wont work :(
 
-class ClientInstance {
-public:
-	//static std::map<uint64_t, bool> ciKeymap;
+	class ClientInstance {
+	public:
+		//static std::map<uint64_t, bool> ciKeymap;
 
-private:
-	char pad_0x0000[0xA8]; //0x0000
-public:
-	MinecraftGame* mcGame; //0x00A8
-	TimerClass* timerClass; //0x00B0
-private:
-	char pad_0x00B8[0x18]; //0x00B8
-public:
-	LoopbackSender* loopbackSender; //0x00D0
-private:
-	char pad_0x00D8[0x60]; //0x00D8
-private:
-	//Actor* localPlayer; //0x0138
-	char pad_0x0138[0x8]; //0x0138
-private:
-	char pad_0x0140[0x3A8]; //0x0140
-public:
-	GuiData* guiData; //0x04E8
+	private:
+		char pad_0x0000[0xA8]; //0x0000
+	public:
+		MinecraftGame* mcGame; //0x00A8
+		TimerClass* timerClass; //0x00B0
+	private:
+		char pad_0x00B8[0x18]; //0x00B8
+	public:
+		LoopbackSender* loopbackSender; //0x00D0
+	private:
+		char pad_0x00D8[0x60]; //0x00D8
+	private:
+		//Actor* localPlayer; //0x0138
+		char pad_0x0138[0x8]; //0x0138
+	private:
+		char pad_0x0140[0x3A8]; //0x0140
+	public:
+		GuiData* guiData; //0x04E8
 
-public:
-	auto getGuiData() {
-		return reinterpret_cast<GuiData*>((uintptr_t)(this) + 0x4E8);
-	};
+	public:
+		auto getGuiData() {
+			return reinterpret_cast<GuiData*>((uintptr_t)(this) + 0x4E8);
+		};
 
-	auto getLevelRender() {
-		return reinterpret_cast<LevelRender*>((uintptr_t)(this) + 0xC0);
-	};
+		auto getLevelRender() {
+			return reinterpret_cast<LevelRender*>((uintptr_t)(this) + 0xC0);
+		};
 
-	auto getFovX() {
-		return reinterpret_cast<float*>((uintptr_t)(this) + 0x670);
-	};
+		auto getFovX() {
+			return reinterpret_cast<float*>((uintptr_t)(this) + 0x670);
+		};
 
-	auto getFovY() {
-		return reinterpret_cast<float*>((uintptr_t)(this) + 0x684);
-	};
+		auto getFovY() {
+			return reinterpret_cast<float*>((uintptr_t)(this) + 0x684);
+		};
 
-	auto getMatrix() {
-		return reinterpret_cast<GLMatrix*>((uintptr_t)(this) + 0x2F0);
-	};
+		auto getMatrix() {
+			return reinterpret_cast<GLMatrix*>((uintptr_t)(this) + 0x2F0);
+		};
 
-	auto getTimerClass() {
-		return reinterpret_cast<class TimerClass*>((uintptr_t)(this) + 0xD0);
-	};
+		auto getTimerClass() {
+			return reinterpret_cast<class TimerClass*>((uintptr_t)(this) + 0xD0);
+		};
 
-	auto getLoopbackSender() {
-		return reinterpret_cast<class LoopbackSender*>((uintptr_t)(this) + 0xB0);
-	};
+		auto getLoopbackSender() {
+			return reinterpret_cast<class LoopbackSender*>((uintptr_t)(this) + 0xB0);
+		};
 
-	auto getMcGame() {
-		return reinterpret_cast<MinecraftGame*>((uintptr_t)(this) + 0xA8);
-	};
+		auto getMcGame() {
+			return reinterpret_cast<MinecraftGame*>((uintptr_t)(this) + 0xA8);
+		};
 
-	auto getEntityList() {
+		auto getEntityList() {
 
-		std::map<uintptr_t, Actor*> cleanMap = std::map<uintptr_t, Actor*>();
+			std::map<uintptr_t, Actor*> cleanMap = std::map<uintptr_t, Actor*>();
 
-		int i = 0;
-		for (auto ent : entityList) {
-			cleanMap[i] = ent.second;
-			i++;
+			int i = 0;
+			for (auto ent : entityList) {
+				cleanMap[i] = ent.second;
+				i++;
+			}
+
+			return cleanMap;
+		};
+
+		auto getCPlayer() { // local player in client instance crashes so please leave this like this lol?
+			Player* plr = nullptr;
+			for (auto ent : entityList) {
+				plr = reinterpret_cast<Player*>(ent.first);
+				break;
+			}
+			return plr;
+		};
+
+		Vector2 WorldToScreen(Vector3 pos, int width, int height) {
+			auto matrix = getMatrix()->matrix;
+
+			Vector4 cc; // cc's im so funny
+			cc.x = pos.x * matrix[0] + pos.y * matrix[4] + pos.z * matrix[8] + matrix[12];
+			cc.y = pos.x * matrix[1] + pos.y * matrix[5] + pos.z * matrix[9] + matrix[13];
+			cc.z = pos.x * matrix[2] + pos.y * matrix[6] + pos.z * matrix[10] + matrix[14];
+			cc.w = pos.x * matrix[3] + pos.y * matrix[7] + pos.z * matrix[11] + matrix[15];
+
+			Vector3 NDC; // Normalized Device Coordinates (Idfk what that is i have to researhcx wtf poedo!!)
+			NDC.x = cc.x / cc.w;
+			NDC.y = cc.y / cc.w;
+
+			Vector2 plrSrn;
+			plrSrn.x = (width / 2 * NDC.x) + (NDC.x + width / 2);
+			plrSrn.y = -(height / 2 * NDC.y) + (NDC.y + height / 2);
+
+			return plrSrn;
 		}
 
-		return cleanMap;
-	};
-
-	auto getCPlayer() { // local player in client instance crashes so please leave this like this lol?
-		Player* plr = nullptr;
-		for (auto ent : entityList) {
-			plr = reinterpret_cast<Player*>(ent.first);
-			break;
+		__forceinline float transformx(const Vector3& p) {
+			auto matrix = getMatrix()->matrix;
+			return p.x * matrix[0] + p.y * matrix[4] + p.z * matrix[8] + matrix[12];
 		}
-		return plr;
-	};
 
-	Vector2 WorldToScreen(Vector3 pos, int width, int height) {
-		auto matrix = getMatrix()->matrix;
+		__forceinline float transformy(const Vector3& p) {
+			auto matrix = getMatrix()->matrix;
+			return p.x * matrix[1] + p.y * matrix[5] + p.z * matrix[9] + matrix[13];
+		}
 
-		Vector4 cc; // cc's im so funny
-		cc.x = pos.x * matrix[0] + pos.y * matrix[4] + pos.z * matrix[8] + matrix[12];
-		cc.y = pos.x * matrix[1] + pos.y * matrix[5] + pos.z * matrix[9] + matrix[13];
-		cc.z = pos.x * matrix[2] + pos.y * matrix[6] + pos.z * matrix[10] + matrix[14];
-		cc.w = pos.x * matrix[3] + pos.y * matrix[7] + pos.z * matrix[11] + matrix[15];
+		__forceinline float transformz(const Vector3& p) {
+			auto matrix = getMatrix()->matrix;
+			return p.x * matrix[2] + p.y * matrix[6] + p.z * matrix[10] + matrix[14];
+		}
 
-		Vector3 NDC; // Normalized Device Coordinates (Idfk what that is i have to researhcx wtf poedo!!)
-		NDC.x = cc.x / cc.w;
-		NDC.y = cc.y / cc.w;
+		inline bool WorldToScreen(Vector3 origin, Vector3 pos, Vector2& screen, Vector2 fov, Vector2 displaySize) {
+			pos.x -= origin.x;
+			pos.y -= origin.y;
+			pos.z -= origin.z;
 
-		Vector2 plrSrn;
-		plrSrn.x = (width / 2 * NDC.x) + (NDC.x + width / 2);
-		plrSrn.y = -(height / 2 * NDC.y) + (NDC.y + height / 2);
+			float x = transformx(pos);
+			float y = transformy(pos);
+			float z = transformz(pos);
 
-		return plrSrn;
-	}
+			if (z > 0) return false;
 
-	__forceinline float transformx(const Vector3& p) {
-		auto matrix = getMatrix()->matrix;
-		return p.x * matrix[0] + p.y * matrix[4] + p.z * matrix[8] + matrix[12];
-	}
+			float mX = (float)displaySize.x / 2.0F;
+			float mY = (float)displaySize.y / 2.0F;
 
-	__forceinline float transformy(const Vector3& p) {
-		auto matrix = getMatrix()->matrix;
-		return p.x * matrix[1] + p.y * matrix[5] + p.z * matrix[9] + matrix[13];
-	}
+			screen.x = mX + (mX * x / -z * fov.x);
+			screen.y = mY - (mY * y / -z * fov.y);
 
-	__forceinline float transformz(const Vector3& p) {
-		auto matrix = getMatrix()->matrix;
-		return p.x * matrix[2] + p.y * matrix[6] + p.z * matrix[10] + matrix[14];
-	}
+			return true;
+		}
 
-	inline bool WorldToScreen(Vector3 origin, Vector3 pos, Vector2& screen, Vector2 fov, Vector2 displaySize) {
-		pos.x -= origin.x;
-		pos.y -= origin.y;
-		pos.z -= origin.z;
-
-		float x = transformx(pos);
-		float y = transformy(pos);
-		float z = transformz(pos);
-
-		if (z > 0) return false;
-
-		float mX = (float)displaySize.x / 2.0F;
-		float mY = (float)displaySize.y / 2.0F;
-
-		screen.x = mX + (mX * x / -z * fov.x);
-		screen.y = mY - (mY * y / -z * fov.y);
-
-		return true;
-	}
-
-public: // Custom Voids
-	auto isInGame() {
-		return guiData->windowData->inWorld;
+	public: // Custom Voids
+		auto isInGame() {
+			return guiData->windowData->inWorld;
+		};
 	};
 };
