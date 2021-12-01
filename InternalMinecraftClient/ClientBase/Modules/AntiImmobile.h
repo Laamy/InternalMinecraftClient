8289ef1f30481126c9e7a50d94c004d34aa44890
NRG <jcazman19@yahoo.com>
2021-11-30 19:24:55 -0500

    Better Sig

--- InternalMinecraftClient/ClientBase/Modules/AntiImmobile.h
+++ InternalMinecraftClient/ClientBase/Modules/AntiImmobile.h
@@ -4,7 +4,7 @@ class AntiImmobile : public Module {
 public:
 	VirtualFuncHook* func;
 	AntiImmobile(std::string cat) : Module(cat, "AntiImmobile", "Stops the server from making you immobile", 0x07) {
-		uintptr_t address = Mem::findSig("48 89 5C 24 08 48 89 74 24 10 57 48 83 EC 30 48 8D 54 24 20 E8 57 46 F5 FE 90 48 8B C8 E8 5E E6 93 FE 48 8B D8 48 8B C8 E8 03 D4 11 00 84 C0 75 15"); //when the intop is smart :Flushed:
+		uintptr_t address = Mem::findSig("48 ? 5C 24 ? ? ? 74 ? ? 57 48 83 EC ? 48 8D ? 24 ? E8 ? ? ? ? 90 ? ? C8 ? ? ? ? ? 48 8B ? ? 8B C8 E8 ? ? ? ? 84"); //when the intop is smart :Flushed:
 		func = hooks->createHook("isImmobile", address, isImmobile);
 
 	}
