b55367100025b418a0a927c189f4aea6d32c566a
NRG <jcazman19@yahoo.com>
2021-12-02 17:48:49 -0500

    Better sig IG

--- InternalMinecraftClient/ClientBase/Modules/NoAnimations.h
+++ InternalMinecraftClient/ClientBase/Modules/NoAnimations.h
@@ -4,7 +4,7 @@ class NoAnimations : public Module {
 public:
 	VirtualFuncHook* func;
 	NoAnimations(std::string cat) : Module(cat, "NoAnimations", "Stop basic player animations", 0x07) {
-		uintptr_t address = Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? ? ? 70 B8 ? ? 78 A8 44 ? ? 40 98 44 ? ? 48 88 44 ? ? 90 ? ? ? ? 44 ? ? 98 ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B EA 4C 8B F1 48 8B 89 ? ? ? ? 49 8B 86 ? ? ? ? 48 2B C1");
+		uintptr_t address = Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 44 0F 29 48 ? 44 0F 29 90 ? ? ? ? 44 0F 29 98 ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B EA");
 		func = hooks->createHook("OnAnimationRender", address, onAnimationRender);
 	};
 
