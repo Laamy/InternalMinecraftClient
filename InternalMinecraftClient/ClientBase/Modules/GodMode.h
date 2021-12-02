cd711fef4d626fe5c5250e4b2c3852c45fea2e39
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-02 18:13:39 -0500

    Update GodMode.h

--- InternalMinecraftClient/ClientBase/Modules/GodMode.h
+++ InternalMinecraftClient/ClientBase/Modules/GodMode.h
@@ -5,6 +5,8 @@ public:
 	Godmode(std::string cat) : Module(cat, "Godmode", "LocalWorld only Godmode!", 0x07) {}
 
 	void OnGameTick(Actor* a1) override {
+		a1->HurtTime = 0;
+		a1->fallDistance = 0.f;
 		a1->CanTakeDamage = false;
 	}
 };
