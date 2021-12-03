25afe35154336b1aa63dbac78fda8f7c71d89e05
NRG <jcazman19@yahoo.com>
2021-12-02 21:30:24 -0500

    People dont like crasher so its just this now until we can make an actual one

--- InternalMinecraftClient/ClientBase/Modules/Crasher.h
+++ InternalMinecraftClient/ClientBase/Modules/Crasher.h
@@ -5,7 +5,7 @@ public:
 	Crasher(std::string cat) : Module(cat, "Crasher", "Supposed To Crashes Realms, But Crashes You...", 0x07) {};
 
 	void OnGameTick(Actor* lp){
-		lp->setPos(Vector3{99999999.f,99999999.f,99999999.f});
+		lp->setPos(Vector3{0.f,90.f,0.f});
 	}
 
 };
