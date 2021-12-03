5c90705093e0d3c8eaf7a04f926c5771f4098ce3
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-03 17:35:20 -0500

    Update Utils.h

--- InternalMinecraftClient/Utils/Utils.h
+++ InternalMinecraftClient/Utils/Utils.h
@@ -92,4 +92,10 @@ public:
             };
         };
     };
+    static void patchBytes(BYTE* dst, BYTE* src, unsigned int size) {
+        DWORD oldprotect;
+        VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+        memcpy(dst, src, size);
+        VirtualProtect(dst, size, oldprotect, &oldprotect);
+    }
 };
