8803f0dc26395acffc58038ca2dd50324a01b1fa
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-03 20:32:45 -0500

    jumpFromGround() works now

--- InternalMinecraftClient/ClientBase/Modules/Bhop.h
+++ InternalMinecraftClient/ClientBase/Modules/Bhop.h
@@ -10,28 +10,28 @@ public:
 			Vector3 po = Vector3(cos((yaw + 90) * 0.01745329251f) * 0.44f, player->Velocity.y, sin((yaw + 90) * 0.01745329251f) * 0.44f);
 			player->lerpMotion(po);
 			if (player->onGround) {
-				player->Velocity.y = 0.40f;
+				player->jumpFromGround();
 			}
 		}
 		if (keymap['A']) {
 			Vector3 po = Vector3(cos((yaw + 0) * 0.01745329251f) * 0.44f, player->Velocity.y, sin((yaw + 0) * 0.01745329251f) * 0.44f);
 			player->lerpMotion(po);
 			if (player->onGround) {
-				player->Velocity.y = 0.40f;
+				player->jumpFromGround();
 			}
 		}
 		if (keymap['S']) {
 			Vector3 po = Vector3(cos((yaw - 90) * 0.01745329251f) * 0.44f, player->Velocity.y, sin((yaw - 90) * 0.01745329251f) * 0.44f);
 			player->lerpMotion(po);
 			if (player->onGround) {
-				player->Velocity.y = 0.40f;
+				player->jumpFromGround();
 			}
 		}
 		if (keymap['D']) {
 			Vector3 po = Vector3(cos((yaw + 180) * 0.01745329251f) * 0.44f, player->Velocity.y, sin((yaw + 180) * 0.01745329251f) * 0.44f);
 			player->lerpMotion(po);
 			if (player->onGround) {
-				player->Velocity.y = 0.40f;
+				player->jumpFromGround();
 			}
 		}
 	}
