155d80695b859af3e76130f9fe2207625ab3d802
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-03 19:02:34 -0500

    Update NoShadow.h

--- InternalMinecraftClient/ClientBase/Modules/NoShadow.h
+++ InternalMinecraftClient/ClientBase/Modules/NoShadow.h
@@ -2,19 +2,14 @@
 
 class NoShadow : public Module {
 public:
-	VirtualFuncHook* func;
 	NoShadow(std::string cat) : Module(cat, "NoShadow", "Stop rendering annoying shadows", 0x07) {
-		uintptr_t address = Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? ? ? 70 B8 ? ? 78 A8 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B F9 48 8D BA");
-		func = hooks->createHook("OnShadowRender", address, onShadowRender);
-	};
+	}
 
-	void OnEnable(ClientInstance* a1, Actor* a2) override {
-		func->enableHook();
-	};
+	virtual void OnGameTick(Actor* lp) override {
+		lp->setShadowRadius(0.f);
+	}
 
-	void OnDisable(ClientInstance* a1, Actor* a2) override {
-		func->disableHook();
-	};
-
-	static void onShadowRender(void* a1, void* a2) { return; };
+	void OnDisable(ClientInstance* a1, Actor* lp) override {
+		lp->setShadowRadius(0.60f);
+	}
 };
