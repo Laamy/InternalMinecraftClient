f165e96172a5a4f282d68f249d95c61929bae89c
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-05 14:00:36 -0500

    Pearl please stop

--- InternalMinecraftClient/ClientBase/Commands/XpCommand.h
+++ InternalMinecraftClient/ClientBase/Commands/XpCommand.h
@@ -7,12 +7,12 @@ public:
 	void Execute(std::vector<std::string> args) {
 		auto lp = clientInst->getLocalPlayer();
 
-		if (!empty(args[1]) && isNumber(args[1])) {
+		/*if (!empty(args[1]) && isNumber(args[1])) {
 			int amount = std::stoi(args[1]);
 			lp->addLevels(amount);
 			lp->displayClientMessage("[Trero Internal] Succesfully gave " + std::string (args[1]) + " level(s)!");
 		}else
 		    lp->addLevels(100);
-		    lp->displayClientMessage("[Trero Internal] Succesfully gave 100 level(s)!");
+		    lp->displayClientMessage("[Trero Internal] Succesfully gave 100 level(s)!");*/
 	}
 };
