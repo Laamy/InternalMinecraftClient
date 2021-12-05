42a4e15e28edaf6b273a77f88ee554446c044e6d
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-05 14:05:28 -0500

    wildcarded

--- InternalMinecraftClient/ClientBase/Modules/ShulkerNest.h
+++ InternalMinecraftClient/ClientBase/Modules/ShulkerNest.h
@@ -4,7 +4,7 @@ class ShulkerNest : public Module {
 public:
 	VirtualFuncHook* func;
 	ShulkerNest(std::string cat) : Module(cat, "ShulkerNest", "Allows you to put shulker boxes in shulker boxes", 0x07) {
-		uintptr_t address = Mem::findSig("48 89 5C 24 08 57 48 83 EC 20 48 8B 41 08 48 8B F9 48 85 C0 74");
+		uintptr_t address = Mem::findSig("48 89 5C 24 ? 57 48 83 EC ? 48 8B 41 ? 48 8B F9 48 85 C0 74");
 		func = hooks->createHook("ShulkerNest", address, itemAllowed);
 	}
 
