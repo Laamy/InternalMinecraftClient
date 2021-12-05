f3d2512374fb3fbee51e4f9e100d2fbc90190dcc
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-12-05 19:51:26 +0100

    Update XpCommand.h

--- InternalMinecraftClient/ClientBase/Commands/XpCommand.h
+++ InternalMinecraftClient/ClientBase/Commands/XpCommand.h
@@ -7,7 +7,11 @@ public:
 	void Execute(std::vector<std::string> args) {
 		auto lp = clientInst->getLocalPlayer();
 
-		lp->addLevels(100);
-		lp->displayClientMessage("Succesfully gave 100 levels!!!!");
+		if (!empty(args[1]) && isNumber(args[1])) {
+			int amount = std::stoi(args[1]);
+			lp->addLevels(amount);
+		}else
+		    lp->addLevels(100);
+		    lp->displayClientMessage("[Trero Internal] Succesfully gave " + std::string (args[1]) + " level(s)!");
 	}
 };
