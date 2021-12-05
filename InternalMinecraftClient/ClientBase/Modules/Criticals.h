61ed3fb46049150d85f9d9da4b92a46b67e5ecd6
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-05 09:54:04 -0500

    Sexy Crits

--- InternalMinecraftClient/ClientBase/Modules/Criticals.h
+++ InternalMinecraftClient/ClientBase/Modules/Criticals.h
@@ -8,21 +8,21 @@ public:
 		//func = hooks->createHook("Criticals", address, Crits);
 
 	}
-
+	void* targetAddress;
 	void OnEnable(ClientInstance* a1, Actor* a2) override {
-		//func->enableHook();
+		if (targetAddress == nullptr)
+			targetAddress = (void*)Mem::findSig("73 5A 80 B9 D8 01 ? ? ? 75 51");
+		BYTE* patch = (BYTE*)"\x90\x90\x90\x90\x90\x90\x90\x90\x90";
+		Mem::patchBytes((BYTE*)((uintptr_t)targetAddress), patch, 9);
 	}
 	void OnGameTick(Actor* a2) {
-		a2->fallDistance = 0.9f;
+		/*a2->fallDistance = 0.9f;
 		if (a2->onGround)
-			a2->Velocity.y = 0.05f;
+			a2->Velocity.y = 0.05f;*/
 	}
 
 	void OnDisable(ClientInstance* a1, Actor* a2) override {
-		//func->disableHook();
+		BYTE* patch = (BYTE*)"\x73\x5A\x80\xB9\xD8\x01\x00\x00\x00";
+		Mem::patchBytes((BYTE*)((uintptr_t)targetAddress), patch, 9);
 	}
-
-	//static bool Crits(Actor* lp) {
-	//	return false;
-	//}
 };
