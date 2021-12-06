aa57c6ae7c35bb97b7192a5e896c1071ef7d76ed
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-06 16:23:04 -0500

    fix

--- InternalMinecraftClient/SDK/GameMode.h
+++ InternalMinecraftClient/SDK/GameMode.h
@@ -4,19 +4,19 @@ class GameMode {
 public:
 	class Actor* actorPtr; //0x0008
 	virtual void Destructor();
-	virtual void startDestroyBlock(struct BlockPos const& pos, unsigned char blockSide, bool& isDestroyedOut);
-	virtual void destroyBlock(struct BlockPos*, unsigned char);
-	virtual void continueDestroyBlock(struct BlockPos const&, unsigned char blockSide, bool& isDestroyedOut);
-	virtual void stopDestroyBlock(struct BlockPos const&);
-	virtual void startBuildBlock(struct BlockPos const&, unsigned char);
-	virtual void buildBlock(struct BlockPos*, unsigned char);
-	virtual void continueBuildBlock(struct BlockPos const&, unsigned char);
+	virtual void startDestroyBlock(struct iVector3 const& pos, unsigned char blockSide, bool& isDestroyedOut);
+	virtual void destroyBlock(struct iVector3*, unsigned char);
+	virtual void continueDestroyBlock(struct iVector3 const&, unsigned char blockSide, bool& isDestroyedOut);
+	virtual void stopDestroyBlock(struct iVector3 const&);
+	virtual void startBuildBlock(struct iVector3 const&, unsigned char);
+	virtual void buildBlock(struct iVector3*, unsigned char);
+	virtual void continueBuildBlock(struct iVector3 const&, unsigned char);
 	virtual void stopBuildBlock(void);
 	virtual void tick(void);
 	virtual float getPickRange();
 	virtual void useItem();
 	virtual void useItemOn();
-	virtual void interact(Actor*, struct Vec3*);
+	virtual void interact(Actor*, struct Vector3*);
 	virtual void attack(Actor*);
 	virtual void releaseUsingItem();
 	virtual void setTrialMode(bool);
