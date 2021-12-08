3f32cbe495d0859a10c5fbb4a3a787af108c1295
NRG <jcazman19@yahoo.com>
2021-12-08 14:29:22 -0500

    Auto close lol

--- InternalMinecraftClient/ClientBase/Modules/ChestDumper.h
+++ InternalMinecraftClient/ClientBase/Modules/ChestDumper.h
@@ -8,6 +8,16 @@ public:
 		for (int i = 0; i < 56; i++){
 			a1->shiftClickItems("inventory_items", i);
 			a1->shiftClickItems("hotbar_items", i);
+			while (true)
+			{
+				keybd_event(VK_ESCAPE, 0,
+					KEYEVENTF_EXTENDEDKEY | 0,
+					0);
+				keybd_event(VK_ESCAPE, 2,
+					KEYEVENTF_KEYUP | 2,
+					2);
+				break;
+			}
 		}
 	}
 };
--- InternalMinecraftClient/ClientBase/Modules/ChestStealer.h
+++ InternalMinecraftClient/ClientBase/Modules/ChestStealer.h
@@ -5,7 +5,18 @@ public:
 	ChestStealer(std::string cat) : Module(cat, "ChestStealer", "Takes everything out of a chest", 0x07) {};
 
 	void OnContainerTick(ContainerManagement* a1, Actor* lp) {
-		for (int i = 0; i < 56; i++)
+		for (int i = 0; i < 56; i++) {
 			a1->shiftClickItems("container_items", i);
+			while (true)
+			{
+				keybd_event(VK_ESCAPE, 0,
+					KEYEVENTF_EXTENDEDKEY | 0,
+					0);
+				keybd_event(VK_ESCAPE, 2,
+					KEYEVENTF_KEYUP | 2,
+					2);
+				break;
+			}
+		}
 	}
 };
