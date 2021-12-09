74ad9d069e5e76ebd6a52dee3eb4c89468d108f5
NRG <jcazman19@yahoo.com>
2021-12-09 18:38:27 -0500

    Still currently working on this

--- InternalMinecraftClient/SDK/Packet/Packet.h
+++ InternalMinecraftClient/SDK/Packet/Packet.h
@@ -29,3 +29,45 @@ public:
 		return *(uint64_t*)(reinterpret_cast<uintptr_t>(this) + 0x0);
 	}
 };
+
+//Still working on currently
+class PlayerAuthInputPacket {
+public:
+    uint64_t VTable;
+    uint64_t num4294967298;
+    char padThingy[32];
+    Vector2 bodyRot;
+    Vector3 position;
+    float headYaw;
+    Vector3 velocity;
+    float InputAD; // 1 for A, -1 for D, multiply by sqrt(2)/2 if mixed with InputWS 
+    float InputWS; // 1 for W, -1 for S, multiply by sqrt(2)/2 if mixed with InputAD 
+    uint8_t epicpad[12];
+    uint32_t inputKeys;
+    int zero;
+    int one;
+    int two;
+    int counter;
+
+    PlayerAuthInputPacket(Actor* Player, Vector3 position, Vector2 bodyRot) {
+        //this->VTable = Minecraft.Windows.exe + 3EB98A8;
+        for (int i = 0; i < 32; i++) {
+            this->padThingy[i] = 0;
+        }
+        this->num4294967298 = 4294967298;
+        this->position = position;
+        this->bodyRot = bodyRot;
+        this->headYaw = bodyRot.y;
+        //this->velocity = Player->Velocity;
+        this->InputAD = 0.f;
+        this->InputWS = 0.f;
+        for (int i = 0; i < 12; i++) {
+            this->epicpad[i] = 0;
+        }
+        this->inputKeys = 0;
+        this->zero = 0;
+        this->one = 1;
+        this->two = 2;
+        this->counter = 0;
+    }
+};
