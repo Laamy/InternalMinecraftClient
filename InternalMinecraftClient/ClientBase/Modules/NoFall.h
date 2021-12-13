8cf1315f638742e3cd2a94d8f734e9440f5a3bdc
Intoprelised <78622918+Intoprelised@users.noreply.github.com>
2021-12-13 14:10:19 -0500

    Update NoFall.h

--- InternalMinecraftClient/ClientBase/Modules/NoFall.h
+++ InternalMinecraftClient/ClientBase/Modules/NoFall.h
@@ -5,10 +5,11 @@ public:
 	NoFall(std::string cat) : Module(cat, "NoFall", "Disable falldamage in LocalWorlds", 0x07) {};
 
 	void OnGameTick(Actor* lp) override {
+		lp->fallDistance = 0.f;/*
 		if (lp->fallDistance > 2.5f) {
 			lp->tryTeleportTo(*lp->getPos(), true, true, 1, 1);
 			lp->onGround = true;
 			lp->fallDistance = 0.f;
-		}
+		}*///sorry pearl its good but this isint a nofall people are looking for, once we have enums we can add this as an option. I like it but sorry :(
 	}
 };
