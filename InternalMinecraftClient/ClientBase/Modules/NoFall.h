f502872b86007cccde0cbea534c7a2c0ebe647d2
zPearlss <87713241+zPearlss@users.noreply.github.com>
2021-12-13 15:33:21 +0100

    Big brain Nofall

--- InternalMinecraftClient/ClientBase/Modules/NoFall.h
+++ InternalMinecraftClient/ClientBase/Modules/NoFall.h
@@ -5,6 +5,10 @@ public:
 	NoFall(std::string cat) : Module(cat, "NoFall", "Disable falldamage in LocalWorlds", 0x07) {};
 
 	void OnGameTick(Actor* lp) override {
-		lp->fallDistance = 0.f;
+		if (lp->fallDistance > 2.5f) {
+			lp->tryTeleportTo(*lp->getPos(), true, true, 1, 1);
+			lp->onGround = true;
+			lp->fallDistance = 0.f;
+		}
 	}
 };
